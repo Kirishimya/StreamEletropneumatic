2311bff8ec26c7f1531d5a59a52eb47041e678dd
Kirishimya <59265149+Kirishimya@users.noreply.github.com>
2022-05-09 10:57:25 -0300

    Teste de acionamento

---
+++ teste_acionamento/MyExceptions.h
@@ -0,0 +1,12 @@
+#ifndef MYEXCEPTIONS_H
+#define MYEXCEPTIONS_H
+#include "TriggersAndCMs.h"
+using namespace trs_and_cms;
+//namespace my_execeptions{
+//   class MyExceptions: public exception{
+//     virtual const char* what(uint8_t tt) const throw(){
+//       return String(tt)+" não é um tipo de gatilho.";
+//     }
+//   };
+//}
+#endif
